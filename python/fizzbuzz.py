7870df950a94987225360becf21d4c7f0c27479e
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-04-07 16:24:22 +1000

    added rules

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,22 +1,34 @@
+
+# Objective:
+# 1. If the number is divisible by 3, print Fizz instead of the number
+# 2. If the number is divisible by 5, print Buzz instead of the number
+# 3. If the number is divisible by 3 and 5 both, print FizzBuzz instead of the number
+
 import time
 
-start_time = time.time_ns()
 
-# standard iteration approach : 0.23 ms
-for i in range(1, 101):
-    output = ""
-    if i % 3 == 0:
-        output += "Fizz"
-    if i % 5 == 0:
-        output += "Buzz"
-    if len(output) > 0:
-        print(output)
-    else:
-        print(f"{i}")
+def main():
+    start_time = time.time_ns()
 
-# lambda approach : 0.23 ms
-# print(*map(lambda i: 'Fizz'*(not i % 3)+'Buzz'*(not i % 5) or i, range(1, 101)), sep='\n')
+    # iteration approach : 0.23 ms
+    for i in range(1, 101):
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        if len(output) > 0:
+            print(output)
+        else:
+            print(f"{i}")
 
-time_taken = float(time.time_ns() - start_time) / 1000000.0
+    # lambda approach : 0.23 ms
+    # print(*map(lambda i: 'Fizz'*(not i % 3)+'Buzz'*(not i % 5) or i, range(1, 101)), sep='\n')
 
-print(f"\nTime taken: {time_taken} ms")
+    time_taken = float(time.time_ns() - start_time) / 1000000.0
+
+    print(f"\nTime taken: {time_taken} ms")
+
+
+if __name__ == '__main__':
+    main()
